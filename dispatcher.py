9b6beceda8f58036fd8cf1ce1b66728d777d56ce
Jonny Li <li2068@purdue.edu>
2022-03-21 17:00:22 -0400

    adding governer, which running on edge devices script execution

--- dispatcher.py
+++ dispatcher.py
@@ -14,16 +14,19 @@ def createSSHClient(server, password):
 
 #The dispatch function should be called when one application instance is orchestrated
 def dispatch(directory, allocation,edge_list,task_dict, instance_count, dependency_dic,inputfile_dic):
-	allocation_file = "allocation_"+str(instance_count)+".json"
+	allocation_file = os.path.join(directory,"allocation_"+str(instance_count)+".json")
 	with open(allocation_file,'w') as allocate:
 		allocate.write(json.dumps(allocation))
+	allocate.close()
 	for each_task in allocation.keys():
 		print("task {} is allocated to edge device {}".format(each_task,allocation[each_task]))
 		file_path=os.path.join(directory,task_dict[each_task])
 		des_file = task_dict[each_task].split('.')[0]+"_"+str(instance_count)+".py"
+		#llocation_file_path = os.path
 		for each_device in allocation[each_task]:
+			print(file_path,des_file)
 			edge_list[each_device].put(file_path, des_file)
-	print(inputfile_dic)
+			edge_list[each_device].put(allocation_file)
 	for each in inputfile_dic:
 		if inputfile_dic[str(each)][0][1]==0:
 			input_path = os.path.join(directory,inputfile_dic[str(each)][0][0])
