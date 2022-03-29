c905590c5406bade768faa8c1ff00ee7973b8457
Jonny Li <li2068@purdue.edu>
2022-03-28 22:56:36 -0400

    2 instance works

--- governer.py
+++ governer.py
@@ -84,6 +84,7 @@ if __name__ =='__main__':
 	task_dic=json_file_loader("task_file.json")
 	depend_lookup=json_file_loader("depend_lookup.json")
 	input_lookup=json_file_loader("input_lookup.json")
+	output_lookup=json_file_loader("output_lookup.json")
 
 	#print(input_lookup)
 	#print(depend_lookup)
@@ -99,26 +100,41 @@ if __name__ =='__main__':
 		print("task {} did not finish execution, exiting!".format(task))
 		sys.exit()
 
+	output_from_current_tk = output_lookup[str(args.tk)]
+	for each in output_from_current_tk:
+		intermediate_file = each[0]+str(instance_count)+each[2]
+		print(intermediate_file)
+		print(each)
+		edge_list_scp[allocation_dic[str(each[3])][0]].put(intermediate_file)
+
 	#looking for the dependency and find the next device and task
 	next_stage_dict=next_task(args.tk,depend_lookup,input_lookup)
+	print(next_stage_dict)
 	#Curent task is the last task
 	if len(next_stage_dict) == 0:
 		print(input_lookup['end'])
 		result_file = "predict_"+str(args.ic)+".txt"
 		edge_list_scp[3].put(result_file,"/home/jonny/Documents/Research/IBDASH_V2")
 	#send the output from this stage to next device
+
 	for each_tk in next_stage_dict.keys():
 		#print(each_tk)
 		ed = allocation_dic[each_tk][0]
 		#print(next_stage_dict[each_tk])
 		# drop the input file to the designated device
-		infile = next_stage_dict[each_tk][0][0].split('.')[0]+"_"+str(instance_count)+".txt"
-		edge_list_scp[ed].put(infile)
+		for each_input in next_stage_dict[each_tk]:
+			print(each_input)
+			if each_input[1] == 1: #checking the second position first to see if it is a intermediate generated file
+				infile = each_input[0]+str(instance_count)+each_input[2]
+				if os.path.exists(infile):
+					edge_list_scp[ed].put(infile)
+					print("moving {} to ed : {}".format(infile,ed))
 		allocation_file = "allocation_"+str(args.ic)+".json"
 		command = "python governer.py --all {} --tk {} --ic {}".format(allocation_file,each_tk,args.ic)
-		#print(command)
+		print(command)
+		print("the above command execute on ed: {}".format(ed))
 		stdin,stdout,stderr=edge_list_ssh[ed].exec_command(command)
-		for line in stdout.read().splitlines():
+		for line in stderr.read().splitlines():
 			print(line)
 		# start the execution 
 
