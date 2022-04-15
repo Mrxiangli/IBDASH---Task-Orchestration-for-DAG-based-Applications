9e477143770da98dbc74f06da439965e602f8114
Jonny Li <li2068@purdue.edu>
2022-04-15 16:02:54 -0400

    p2p

--- back.py
+++ back.py
@@ -17,19 +17,24 @@ def createSSHClient(server, password):
     else:
         client.connect(server,username='johnny')
     client_scp = SCPClient(client.get_transport())
-    return client_scp
+    return client_scp, client
 
 
 scp_list=[]
+ssh_list=[]
 
 server_list = [("54.172.191.10","IBDASH_V2.pem"),("3.234.212.152","IBDASH_V2.pem"),("3.228.0.215","IBDASH_V2.pem"),("128.46.32.175","pass.pd")]
 for each in server_list:
-    scp_list.append(createSSHClient(each[0],each[1]))
+    scp,ssh = createSSHClient(each[0],each[1])
+    scp_list.append(scp)
+    ssh_list.append(ssh)
 
 # client = paramiko.SSHClient()
 # client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 # client.connect("128.46.32.175",username='johnny', key_filename="IBDASH_V2.pem")
 # client_scp = SCPClient(client.get_transport())
+for each in ssh_list:
+    each.exec_command("rm *.npy & rm *.txt & rm *.py & rm *.json")
 
 for each in scp_list:
     start = timer.time()
@@ -37,3 +42,5 @@ for each in scp_list:
     each.put("/home/jonny/Documents/Research/IBDASH_V2/edge_list.json")
     end = timer.time()
     print("transfer time: {}".format(end-start))
+
+
--- e2e.py
+++ e2e.py
@@ -1,32 +1,40 @@
 import os.path
 import subprocess
+import argparse
+import configparser
 
-instance_count = 1
+if __name__ =='__main__':
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--c', type=int, nargs="?")
+	parser.add_argument('--f', type=str, nargs="?")
+	args = parser.parse_args()
 
-result_list = []
-for i in range(1, instance_count+1):
-	file = "predict_{}.txt".format(i)
-	result_list.append(file) 
+	instance_count = args.c
 
-current_dir = os.getcwd()
-timefile=os.path.join(current_dir,"time.txt")
+	result_list = []
+	for i in range(1, instance_count+1):
+		file = "predict_{}.txt".format(i)
+		result_list.append(file) 
 
-time_list = []
+	current_dir = os.getcwd()
+	timefile=os.path.join(current_dir,args.f)
 
-while result_list != []:
-	for each in result_list:
-		if os.path.exists(each):
-			p=subprocess.Popen(["date +%s%N"],shell=True,stdin=None,stdout=subprocess.PIPE,stderr=subprocess.PIPE,close_fds=True)
-			out,err = p.communicate()
-			result_list.remove(each)
-			instance = each.split('_')[1].split('.')[0]
-			time_list.append([instance,out.decode("utf-8")])
-			
-time_file = open(timefile,"a")
+	time_list = []
 
-for each in time_list:
-	instance,out = each
-	time_file.write("instance {} end:".format(instance))
-	time_file.write(out)
-time_file.close()
+	while result_list != []:
+		for each in result_list:
+			if os.path.exists(each):
+				p=subprocess.Popen(["date +%s%N"],shell=True,stdin=None,stdout=subprocess.PIPE,stderr=subprocess.PIPE,close_fds=True)
+				out,err = p.communicate()
+				result_list.remove(each)
+				instance = each.split('_')[1].split('.')[0]
+				time_list.append([instance,out.decode("utf-8")])
+				
+	time_file = open(timefile,"a")
+
+	for each in time_list:
+		instance,out = each
+		time_file.write("instance {} end:".format(instance))
+		time_file.write(out)
+	time_file.close()
 
